bad0188d23d5dc1233cefc08af4d930714112a00
bladeyrj <bladeyrj@foxmail.com>
2019-09-23 20:56:34 +0800

    use pandas to parase data

--- data_reader.py
+++ data_reader.py
@@ -1,48 +1,17 @@
-import xlrd
-data_file = "./data/Simulated Data.xlsx"
+import pandas as pd
+import numpy as np
+
+data_file = "./data/SimulatedData.xlsx"
 
 class dataReader(object):
     def __init__(self):
         self.data = []
 
     def read_file(self, file):
-        with xlrd.open_workbook(file) as workbook:
-            sheet = workbook.sheet_by_index(0)
-            print("Sheet Name:", sheet.name)
-            print("Rows: :", sheet.nrows)
-            print("Columns: :", sheet.ncols)
-            dic_sample = {"A":[0.0 for _ in range(7)],
-                          "B":[0.0 for _ in range(7)],
-                          "C":[0.0 for _ in range(7)],
-            }
-            total_weeks = int(sheet.cell(-1, 0)) * int(sheet.cell(-1, 1))
-            data = [dic_sample for _ in range(total_weeks)]
-
-            for i in range(1, sheet.nrows): # skip the index line
-                # trun month into week
-                week = int(sheet.cell(i, 0)) * int(sheet.cell(i, 1))
-                values = []
-                for j in range(3, 10): # value of buyer 1-7
-                    values.append(float(sheet.cell(i, j)))
-                if sheet.cell(i, 2) == "A":
-                    data[-1]["A"] = values
-                    for j in range(0, 8): #record the return value
-                        if float(sheet.cell(i, j+10)) > 0.0:
-                            returnsA.append(float(sheet.cell(i, j+10)))
-                elif sheet.cell(i, 2) == "B":
-                    data[-1]["A"] = values
-                    for j in range(0, 8): #record the return value
-                        if float(sheet.cell(i, j+10)) > 0.0:
-                            returnsB.append(float(sheet.cell(i, j+10)))
-                elif sheet.cell(i, 2) == "C":
-                    data[-1]["A"] = values
-                    for j in range(0, 8): #record the return value
-                        if float(sheet.cell(i, j+10)) > 0.0:
-                            returnsC.append(float(sheet.cell(i, j+10)))
-                else:
-                    print("Wrong SKU")
-                    break
-            self.data = data
+        file = "./data/SimulatedData.xlsx"
+        data = pd.read_excel(file, sheet_name=0)
+        data["NumOfWeek"] = (data["Season"]-1)*12 + data["Week"]
+        self.data = data
         
     def get_parsed_data(self) -> list:
         return []
